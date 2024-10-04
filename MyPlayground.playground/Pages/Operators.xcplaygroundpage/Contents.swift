2a02d5d1347b88e731e32756802c623562060136
Divya Arora <111882666+divya0o1@users.noreply.github.com>
2024-10-04 23:31:46 +0530

    operators

---
+++ MyPlayground.playground/Pages/Operators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+var a = 2
+var b = 2
+var c = 2
+var d = 4.5
+
+//basic operators
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+//we can do something like this
+print(a*b*c)
+
+//power function
+pow(2, 3)
+print(pow(2, 3))
+
+//square root
+sqrt(4)
+print(sqrt(4))
+
+//round off
+ceil(d)
+print(ceil(d))
+
+//round off(down)
+floor(d)
+print(floor(d))
