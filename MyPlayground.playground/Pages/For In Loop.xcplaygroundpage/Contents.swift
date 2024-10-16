58de72b30effc472a5eedaab3d192da8f71e9233
Divya Arora <111882666+divya0o1@users.noreply.github.com>
2024-10-16 13:48:43 +0530

    for in loop

---
+++ MyPlayground.playground/Pages/For In Loop.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+import UIkit
+
+var str = "Hello Playground"
+
+//for counter in 1...5 {
+////    print("hello")
+//    
+//    var sum = 0
+//    sum += counter
+//    print(sum)
+//}
+var sum = 0
+
+for counter in 1...5 {
+    
+    sum += counter
+    
+}
+
+print(sum)
