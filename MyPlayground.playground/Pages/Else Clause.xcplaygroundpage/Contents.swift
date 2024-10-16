f421bc18b8587a8ad9aa307554a4d97a13d68177
Divya Arora <111882666+divya0o1@users.noreply.github.com>
2024-10-16 13:26:51 +0530

    else clause

---
+++ MyPlayground.playground/Pages/Else Clause.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+import UIkit
+
+var str = "Hello playground"
+
+let a = 25
+
+if a <= 10{
+    print("a is less than 10")
+}
+else if a < 15{
+    print("a is less than 15")
+}
+else if a > 30{
+    print("a is  greater than 30")
+}
+else{
+    print("a is something else")
+}
