8ba1785890a4ac89d0116acd8f837e85a534f4df
Divya Arora <111882666+divya0o1@users.noreply.github.com>
2024-10-18 01:11:57 +0530

    parameter 1

---
+++ MyPlayground.playground/Pages/addTwoNo func return values.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var str = "Hello, Playground "
+
+func addTwoNumbers() -> Int {
+    
+    let a = 1
+    let b = 2
+    let c = a + b
+    
+    return c
+}
+
+let sum = addTwoNumbers()
+print(sum)
